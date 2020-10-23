e317d474331f09f8b69d24e7d08ea1812c131dae
echeftr <69512919+echeftr@users.noreply.github.com>
2020-10-23 20:19:19 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,14 @@
+
+// For loops: even-odd-reporter
+
+for (i = 0; i <= 20; i++) {
+    if (i % 2 ===0) {
+        console.log(i + " is even");
+    } else {
+    console.log(i + " is oneven");
+}
+}
+
+
+
+
---
+++ multiplication-tables.js
@@ -0,0 +1,7 @@
+// For loops: multiplication tables
+for (i = 0; i <= 10; i++) {
+    if (i * 9) {
+        console.log(i + " x 9 = " + i * 9);
+    }
+}
+
